c0c133ba2faead1c6b54106f0e54e80080c9b749
CarlosEduFF <carlos.farias11@etec.sp.gov.br>
2024-03-25 18:01:44 -0300

    Correction of responsive table

--- resources/views/listar.blade.php
+++ resources/views/listar.blade.php
@@ -3,58 +3,65 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Listar</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>  
 </head>
 <body>
-
-
-
-
     <div class="container">
         <div class="row">
             <div class="col">
-            <nav class="navbar navbar-expand-lg bg-primary">
-        <div class="container-fluid">
-            <a class="navbar-brand text-white" href="#">Sistema Web</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                <a class="nav-link active text-white" aria-current="page" href="#">Cadastrar</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link active text-white" aria-current="page" href="#">Consultar</a>
-                </li>
-            </ul>
-            </div>
-        </div>
-        </nav>
-    
+                <nav class="navbar navbar-expand-lg bg-primary">
+                    <div class="container-fluid">
+                        <a class="navbar-brand text-white" href="#">Sistema Web</a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                <a class="nav-link active text-white" aria-current="page" href="#">Cadastrar</a>
+                                </li>
+                                <li class="nav-item">
+                                <a class="nav-link active text-white" aria-current="page" href="#">Consultar</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                    
         <br>
         <h4>Cadastrar - Agendamento de possiveis Clientes</h4>
         <h6>Sistema utilizado para agendamento de serviços.</h6>
         <br>
-        <div class="">
-            <table class="table table-bordered table-secondary">
-            <thead>
-                <tr class="bg-primary">
-                <th scope="col">Nome</th>
-                <th scope="col">Endereço</th>
-                <th scope="col">Bairro</th>
-                <th scope="col">CEP</th>
-                <th scope="col">Cidade</th>
-                <th scope="col">Estado</th>
-                <th scope="col">Ação</th>
-                </tr>
-            </thead>
-            <tbody>
-        
-            </tbody>
-            </table>'
-    </div>  
+            <div class="table-responsive">
+                <table class="table table-bordered table-secondary table-responsive">
+                    <thead>
+                        <tr class="bg-primary">
+                        <th scope="col">Nome</th>
+                        <th scope="col">Endereço</th>
+                        <th scope="col">Bairro</th>
+                        <th scope="col">CEP</th>
+                        <th scope="col">Cidade</th>
+                        <th scope="col">Estado</th>
+                        <th scope="col">Ação</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>                    
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>       
 </body>
 <html>
---
+++ resources/views/teste.blade.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+	<!-- Bootstrap CDN -->
+	<link href= 
+"https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+		rel="stylesheet"
+		integrity= 
+"sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+		crossorigin="anonymous"> 
+	<title>Bootstrap 5 Table Always responsive</title> 
+</head> 
+
+<body> 
+	<div class="container text-center col-6"> 
+		<h1 class="text-success"> 
+			GeeksforGeeks 
+		</h1> 
+		<h2>Bootstrap 5 Table Always responsive</h2> 
+		<div class="table-responsive"> 
+			<table class="table"> 
+				<thead> 
+					<tr> 
+						<td>State</td> 
+						<td>Capital</td> 
+						<td>State</td> 
+						<td>Capital</td> 
+						<td>State</td> 
+						<td>Capital</td> 
+						<td>State</td> 
+						<td>Capital</td> 
+						<td>State</td> 
+						<td>Capital</td> 
+						<td>State</td> 
+						<td>Capital</td> 
+					</tr> 
+				</thead> 
+				<tbody> 
+					<tr class="table-active"> 
+						<td>Uttar Pradesh</td> 
+						<td>Lucknow</td> 
+						<td>Uttar Pradesh</td> 
+						<td>Lucknow</td> 
+						<td>Uttar Pradesh</td> 
+						<td>Lucknow</td> 
+						<td>Uttar Pradesh</td> 
+						<td>Lucknow</td> 
+						<td>Uttar Pradesh</td> 
+						<td>Lucknow</td> 
+						<td>Uttar Pradesh</td> 
+						<td>Lucknow</td> 
+					</tr> 
+					<tr> 
+						<td>Punjab</td> 
+						<td>Chandigarh</td> 
+						<td>Punjab</td> 
+						<td>Chandigarh</td> 
+						<td>Punjab</td> 
+						<td>Chandigarh</td> 
+						<td>Punjab</td> 
+						<td>Chandigarh</td> 
+						<td>Punjab</td> 
+						<td>Chandigarh</td> 
+						<td>Punjab</td> 
+						<td>Chandigarh</td> 
+
+					</tr> 
+					<tr> 
+						<td>Goa</td> 
+						<td>Panaji</td> 
+						<td>Goa</td> 
+						<td>Panaji</td> 
+						<td>Goa</td> 
+						<td>Panaji</td> 
+						<td>Goa</td> 
+						<td>Panaji</td> 
+						<td>Goa</td> 
+						<td>Panaji</td> 
+						<td>Goa</td> 
+						<td>Panaji</td> 
+					</tr> 
+					<tr> 
+						<td>Himachal Pradesh</td> 
+						<td>Shimla</td> 
+						<td>Himachal Pradesh</td> 
+						<td>Shimla</td> 
+						<td>Himachal Pradesh</td> 
+						<td>Shimla</td> 
+						<td>Himachal Pradesh</td> 
+						<td>Shimla</td> 
+						<td>Himachal Pradesh</td> 
+						<td>Shimla</td> 
+						<td>Himachal Pradesh</td> 
+						<td>Shimla</td> 
+					</tr> 
+				</tbody> 
+			</table> 
+		</div> 
+	</div> 
+</body> 
+</html>
